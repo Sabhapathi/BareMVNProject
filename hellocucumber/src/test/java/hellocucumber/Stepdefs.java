785b618c5d5dcd023eeff8fec40719df49e0a8a7
Sabhapathi Akkipalli <Sabhapathi.Akkipalli@clicksoftware.com>
2019-06-14 10:54:34 -0500

    All files added

---
+++ hellocucumber/src/test/java/hellocucumber/Stepdefs.java
@@ -0,0 +1,40 @@
+package hellocucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+    private String today;
+    private String actualAnaswer;
+
+    @Given("today is \"([^\"]*)\"$")
+    public void today_is(String today) {
+       this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnaswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told \"([^\"]*)\"$")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer,actualAnaswer);
+    }
+
+    @Given("^today is Friday$")
+    public void today_is_Friday() {
+        this.today = "Friday";
+    }
+
+
+}
+
+class IsItFriday{
+    static String isItFriday(String today){
+        return "Friday".equals(today)?"TGIF":"Nope";
+    }
+}
